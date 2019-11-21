edf26f639b647cde129d0699f19a2b2368063c9a
Andrew Williams <1williamsaj@gmail.com>
2019-11-21 03:46:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,9 @@ class Pokemon
       VALUES(?, ?)
     SQL
 
-    
+    DB[:conn].execute(sql, self.name, self.grade)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
   end
 
   def self.find
